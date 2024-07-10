1234f08467a6e4722929cfcda7b46911a9ca60ea
undertarke <sangrom2003@gmail.com>
2024-07-10 12:52:13 +0700

    buoi 4

---
+++ node43_backend/es_comonjs/index.js
@@ -0,0 +1,11 @@
+// import {title, ham} from './script.mjs'
+const { title, ham } = require('./script.js')
+
+// một điều kiện phải là module
+console.log(title)
+
+
+// ES module => (ECMA Script module)
+
+// common js 
+
---
+++ node43_backend/es_comonjs/script.js
@@ -0,0 +1,9 @@
+const title = "node 43"
+
+const ham = () => {
+    return "function node"
+}
+
+// export {title, ham }
+module.exports = { title, ham }
+
